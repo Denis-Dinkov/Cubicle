fdd9b39e3d3258474147f916e5bfea3c3a71c423
Denis Dinkov <denisdinkov7@gmail.com>
2023-07-28 21:39:26 +0300

    setup enviorment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('da')
+const express = require('express');
+const config = require('./config/config')
+
+const app = express();
+
+app.listen(config.PORT, ()=> {
+  console.log(`Server is running on ${config.PORT}!`)
+})
+
